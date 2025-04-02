2a0b50b2c9b361d872a2ad5624739f026f7cf2ea
Le Thi Diem My <lethidiemmy961996@gmail.com>
2025-04-02 22:03:18 +0700

    [AIOCodeMathDA][Week7] Unrem insert data

--- preAIO25/AIOCodeMathDA/quanlybanhang.sql
+++ preAIO25/AIOCodeMathDA/quanlybanhang.sql
@@ -4,24 +4,24 @@
 -- SELECT * FROM quanlybanhang.customers;
 
 # • Câu 4: Chèn dữ liệu vào bảng Salespersons.
--- INSERT INTO quanlybanhang.salespersons (SalespersonID, FullName, Email, PhoneNumber) VALUES
--- (1, 'Le A', 'ale@x.com', '0123234098'),
--- (2, 'Nguyen B', 'bnguyen@x.com', '0483294939'),
--- (3, 'Tran C', 'ctran@x.com', '0938293930');
+INSERT INTO quanlybanhang.salespersons (SalespersonID, FullName, Email, PhoneNumber) VALUES
+(1, 'Le A', 'ale@x.com', '0123234098'),
+(2, 'Nguyen B', 'bnguyen@x.com', '0483294939'),
+(3, 'Tran C', 'ctran@x.com', '0938293930');
 
 
 # • Câu 5: Chèn dữ liệu vào bảng Customers.
--- INSERT INTO quanlybanhang.customers(CustomerID, FullName, Email, PhoneNumber) VALUES
--- (1, 'Trieu D', 'dtrieu@x.com', '0125634098'),
--- (2, 'Ly E', 'ely@x.com', '0163234098'),
--- (3, 'Le G', 'gle@x.com', '0973234098');
+INSERT INTO quanlybanhang.customers(CustomerID, FullName, Email, PhoneNumber) VALUES
+(1, 'Trieu D', 'dtrieu@x.com', '0125634098'),
+(2, 'Ly E', 'ely@x.com', '0163234098'),
+(3, 'Le G', 'gle@x.com', '0973234098');
 
 
-# • Câu 6: Chèn dữ liệu vào bảng Orders.
--- INSERT INTO quanlybanhang.orders (OrderID, CustomerID, SalespersonID, OrderDate, TotalAmount) VALUES
--- (1, 1, 1, '2024-12-24', 5000000),
--- (2, 2, 2, '2024-12-25', 3000000),
--- (3, 3, 3, '2024-12-26', 7000000);
+--  • Câu 6: Chèn dữ liệu vào bảng Orders.
+INSERT INTO quanlybanhang.orders (OrderID, CustomerID, SalespersonID, OrderDate, TotalAmount) VALUES
+(1, 1, 1, '2024-12-24', 5000000),
+(2, 2, 2, '2024-12-25', 3000000),
+(3, 3, 3, '2024-12-26', 7000000);
 
 
 # • Câu 7: Liệt kê tất cả các đơn hàng của nhân viên có SalespersonID = 1.
